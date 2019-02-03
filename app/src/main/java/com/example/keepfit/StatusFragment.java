9cff52b15287f8b4ac007d5b5c6f76ed86cee5a4
tiggerdine <tiggerdine@gmail.com>
2019-02-03 16:07:09 +0000

    add floating action button

--- app/src/main/java/com/example/keepfit/StatusFragment.java
+++ app/src/main/java/com/example/keepfit/StatusFragment.java
@@ -1,15 +1,29 @@
 package com.example.keepfit;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class StatusFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_status, container, false);
+        View view = inflater.inflate(R.layout.fragment_status, container, false);
+
+        FloatingActionButton fab = view.findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "My milkshake brings all the boys to the yard.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
 }
